7d7f644f0a083b9f5d809e6f24c49988a8ccda56
Tui Popenoe <tuipopenoe@gmail.com>
2014-05-29 23:41:54 -0500

    PS1

--- ps1a.py
+++ ps1a.py
@@ -1,31 +1,33 @@
 import math
+import sys
 
 def primes(n):
     """ Return a list of primes < n"""
     root = int(math.ceil(math.sqrt(n)))
-
     if n < 2 :
         return False
     if n % 2 == 0:
         return False
-
-    for i in range(3, root):
+    for i in range(2, root):
         if n % i == 0:
             return False
-
     return True
 
-def main():
+def main(filename):
     count = 0
     num = 2
-
+    nums = []
     while count < 1000:
         if primes(num):
+            nums.append(num)
             count += 1
-            print(count,  "\t" , num)
             num += 1
         else:
             num += 1
 
+    f = open(filename, 'w')
+    for item in nums:
+        f.write("%s\n" %item)
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
