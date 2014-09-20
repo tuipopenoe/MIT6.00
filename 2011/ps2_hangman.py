8195df423d5c86e106ba752ae261e999c70a39ac
Tui Popenoe <tuipopenoe@gmail.com>
2014-09-19 22:59:07 -0500

    ps2_hangman.py

--- 2011/ps2_hangman.py
+++ 2011/ps2_hangman.py
@@ -11,13 +11,13 @@ def load_words():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
     
-    Depending on the size of the word list, this function may
-    take a while to finish.
+    Args:
+        None
+    Rets:
+        list of words
     """
     print "Loading word list from file..."
-    # inFile: file
     inFile = open(WORDLIST_FILENAME, 'r', 0)
-    # line: string
     line = inFile.readline()
     # wordlist: list of strings
     wordlist = string.split(line)
@@ -32,4 +32,45 @@ def choose_word(wordlist):
     """
     return random.choice(wordlist)
 
-wordlist = load_words()
+def play_game():
+    wordlist = load_words()
+    available_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+        'z']
+    word = choose_word(wordlist)
+    guesses = len(word) + 2
+    blanks = ['_' for i in range(len(word))]
+    while guesses > 0:
+        print('You have ' + str(guesses) + ' guesses left.')
+        print('Available letters: ' + ''.join(available_letters))
+        letter = raw_input('Please guess a letter: ').lower()
+        #letter = string.lowercase(letter)
+        while len(letter) != 1:
+            letter = raw_input('Please guess a letter: ').lower()
+            #letter = string.lowercase(letter)
+        if letter in word:
+            for i in range(len(word)):
+                if word[i] == letter:
+                    blanks[i] = letter
+            print('Good guess: ' + ''.join(blanks))
+            available_letters.remove(letter)
+        else:
+            print('Oops! That letter is not in my word: ' + ''.join(blanks))
+        guesses -= 1
+        if blanks == word && guesses > 0:
+            print('Congratulations, you won!')
+            break
+        elif guesses < 1:
+            print('You Lose! Word was: ' + word)
+
+def main():
+    play_game()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
