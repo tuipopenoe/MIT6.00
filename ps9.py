7702fecdb90828a7e3f91849ab99f545fbaadef4
Tui Popenoe <tuipopenoe@gmail.com>
2014-09-27 23:59:18 -0500

    ps9.py Schedule Optimization

---
+++ ps9.py
@@ -0,0 +1,101 @@
+#!python2
+# Tui Popenoe
+# ps9.py - Schedule Optimization
+
+SUBJECT_FILENAME = "subjects.txt"
+SHORT_SUBJECT_FILENAME = "shortened_subjects.txt"
+VALUE, WORK = 0, 1
+
+def loadSubjects(filename):
+    """
+    Returns a dictionary mapping subject name to (value, work), where the name
+    is a string and the value and work are integers. The subject information is
+    read from the file named by the string filename. Each line of the file
+    contains a string of the form "name,value,work".
+
+    returns: dictionary mapping subject name to (value, work)
+    """
+
+    output = {}
+    inputFile = open(filename)
+    for line in inputFile:
+        if x[0] and x[1] and x[2]:
+                i = line.split(',')
+                output[i[0]] = (i[1], i[2])
+    return output
+
+def printSubjects(subjects):
+    """
+    Prints a string containing name, value, and work of each subject in
+    the dictionary of subjects and total value and work of all subjects
+    """
+    totalVal, totalWork = 0,0
+    if len(subjects) == 0:
+        return 'Empty SubjectList'
+    res = 'Course\tValue\tWork\n======\t====\t=====\n'
+    subNames = subjects.keys()
+    subNames.sort()
+    for s in subNames:
+        val = subjects[s][VALUE]
+        work = subjects[s][WORK]
+        res = res + s + '\t' + str(val) + '\t' + str(work) + '\n'
+        totalVal += val
+        totalWork += work
+    res = res + '\nTotal Value:\t' + str(totalVal) +'\n'
+    res = res + 'Total Work:\t' + str(totalWork) + '\n'
+    print res
+
+def cmpValue(subInfo1, subInfo2):
+    """
+    Returns True if value in (value, work) tuple subInfo1 is GREATER than
+    value in (value, work) tuple in subInfo2
+    """
+    if(subInfo1[0] > subInfo2[0]):
+        return True
+    return False
+
+def cmpWork(subInfo1, subInfo2):
+    """
+    Returns True if work in (value, work) tuple subInfo1 is LESS than than work
+    in (value, work) tuple in subInfo2
+    """
+    if(subInfo1[0] > subInfo2[1]):
+        return True
+    return False
+
+def cmpRatio(subInfo1, subInfo2):
+    """
+    Returns True if value/work in (value, work) tuple subInfo1 is 
+    GREATER than value/work in (value, work) tuple in subInfo2
+    """
+    if (subInfo1[0] / subInfo1[1) > (subInfo2[0] / subInfo2[1]):
+        return True
+    return False
+
+def greedyAdvisor(subjects, maxWork, comparator):
+    """
+    Returns a dictionary mapping subject name to (value, work) which includes
+    subjects selected by the algorithm, such that the total work of subjects in
+    the dictionary is not greater than maxWork.  The subjects are chosen using
+    a greedy algorithm.  The subjects dictionary should not be mutated.
+
+    subjects: dictionary mapping subject name to (value, work)
+    maxWork: int >= 0
+    comparator: function taking two tuples and returning a bool
+    returns: dictionary mapping subject name to (value, work)
+    """
+    #TODO
+
+def bruteForceAdvisor(subjects, maxWork):
+    """
+    Returns a dictionary mapping subject name to (value, work), which
+    represents the globally optimal selection of subjects using a brute force
+    algorithm.
+
+    subjects: dictionary mapping subject name to (value, work)
+    maxWork: int >= 0
+    returns: dictionary mapping subject name to (value, work)
+    """
+    #TODO
+
+
