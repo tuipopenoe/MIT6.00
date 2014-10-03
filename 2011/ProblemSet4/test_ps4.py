2235a5b0cd5452dc5ee836db7a9d6fec73dd4c5c
Tui Popenoe <tuipopenoe@gmail.com>
2014-10-02 23:39:30 -0500

    test_ps4.py

--- 2011/ProblemSet4/test_ps4.py
+++ 2011/ProblemSet4/test_ps4.py
@@ -32,22 +32,42 @@ def test_random_string():
 
 def test_random_scrambled():
     """Test the random scrambled method in ps4.py"""
-
+    shift = random.randint(1, 27)
+    pass
 
 def test_get_fable_string():
     """Test the get fable string method in ps4.py"""
+    raise NotImplementedError()
 
 def test_build_coder():
     """Test the build coder method in ps4.py"""
+    shift = random.randomint(1, 27)
+    coder = ps4.build_coder(shift)
+    self.assertTrue(isinstance(coder, dict))
+    random_letter = random.choice(string.letters[:25])
+    self.assertEqual(coder[random_letter], chr(ord(random_letter) + shift))
 
 def test_build_encoder():
     """Test the build encoder method in ps4.py"""
+    test_build_coder()
 
 def test_build_decoder():
     """Test the build decoder method in ps4.py"""
+    raise NotImplementedError()
 
 def test_apply_coder():
     """Test the apply coder method in ps4.py"""
+    shift = random.randint(1, 27)
+    coder = ps4.build_coder(shift)
+    for i in range(random.randint(15, 30)):
+        original_string.append(random.choice(string.letters[:26]))
+    shifted_string = ''
+    for i in original_string:
+        shifted_string.append(coder[i])
+    for i in range(len(original_string)):
+        test_string.append(chr(ord(original_string[i])))
+    self.assertEqual(shifted_string, test_string)
 
 def test_apply_shift():
     """Test the apply shift method in ps4.py"""
+    raise NotImplementedError()
